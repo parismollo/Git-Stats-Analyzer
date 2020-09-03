739c2c6f7006016c18ec302f8eb79faa9d670c2f
Colin González <colin.gonzalez@nomadic-labs.com>
2020-09-03 11:27:57 +0200

    Commit: use JGit to parse commits

--- gitrawdata/src/main/java/up/visulog/gitrawdata/Commit.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/Commit.java
@@ -1,10 +1,25 @@
 package up.visulog.gitrawdata;
 
-import java.io.BufferedReader;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import java.io.IOException;
 
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.AnyObjectId;
+
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
 public class Commit {
-    // FIXME: (some of) these fields could have more specialized types than String
+    // AD: FIXME: (some of) these fields could have more specialized types than String
     public final String id;
     public final String date;
     public final String author;
@@ -28,40 +43,47 @@ public class Commit {
     }
 
     /**
-     * Parses a log item and outputs a commit object. Exceptions will be thrown in case the input does not have the proper format.
+     * Transforms a time encoded as long into a string with
+     * the git log format.
      */
-    public static Commit parse(BufferedReader input) {
-        try {
-            var idLine = input.readLine();
-            var idChunks = idLine.split(" ");
-            if (!idChunks[0].equals("commit")) parseError();
-            var id = idChunks[1];
-
-            var authorLine = input.readLine();
-            if (!authorLine.startsWith("Author: ")) parseError();
-            var author = authorLine.substring(8);
-
-            var dateLine = input.readLine();
-            if (!dateLine.startsWith("Date: ")) parseError();
-            var date = dateLine.substring(6);
-
-            var emptyLine = input.readLine();
-            if (!emptyLine.isBlank()) parseError();
-
-            var description = input
-                    .lines() // get a stream of lines to work with
-                    .takeWhile(currentLine -> !currentLine.isBlank()) // take all lines until the first blank one (commits are separated by blank lines)
-                    .map(String::trim) // remove indentation
-                    .reduce("", (accumulator, currentLine) -> accumulator + currentLine); // concatenate everything
-            return new Commit(id, author, date, description);
-        } catch (IOException e) {
-            parseError();
-            return null; // this is supposed to be unreachable, as parseError should never return
-        }
+    static String stringOfTime(long time, TimeZone tz) {
+	var dtfmt =
+	    new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy Z", Locale.US);
+	dtfmt.setTimeZone(tz);
+	dtfmt.format(Long.valueOf(time));
+	return dtfmt.format(Long.valueOf(time));
     }
 
-    // Helper function for generating parsing exceptions. This function *always* quits on an exception. It *never* returns.
-    private static void parseError() {
-        throw new RuntimeException("Wrong commit format.");
+    /**
+     * Transform a JGit revCommit into a regular Commit object.
+     */
+    public static Commit commitOfRevCommit (AnyObjectId id, RevCommit rCommit){
+	var  author = rCommit.getAuthorIdent();
+	var name = author.getName();
+	var email = author.getEmailAddress();
+	var time = author.getWhen().getTime();
+	var timeZone = author.getTimeZone();
+	var commit =
+	    new Commit(id.getName(),
+		       name + " <" + email+">",
+		       stringOfTime(time, timeZone),
+		       rCommit.getFullMessage());
+	return commit;
+    }
+
+
+    /**
+     * Parses a log item and outputs a commit object. Exceptions will
+     * be thrown in case the input does not have the proper format.
+     */
+    public static Commit parse (Repository repo, AnyObjectId id)
+	throws MissingObjectException,
+	       IncorrectObjectTypeException,
+	       IOException {
+	try (RevWalk walk = new RevWalk(repo)) {
+	    RevCommit rCommit = walk.parseCommit(id);
+	    walk.dispose();
+	    return commitOfRevCommit(id, rCommit);
+	}
     }
 }
