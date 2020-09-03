83b7effe077b02a2ff6c49ddadb8eb4fa8412f28
Colin González <colin.gonzalez@nomadic-labs.com>
2020-09-03 11:27:13 +0200

    Gitrawdata: add dependency to JGit into build config

--- gitrawdata/build.gradle.kts
+++ gitrawdata/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/org.eclipse.jgit/org.eclipse.jgit
+    implementation("org.eclipse.jgit:org.eclipse.jgit:5.8.1.202007141445-r")
     testImplementation("junit:junit:4.+")
 }
 
