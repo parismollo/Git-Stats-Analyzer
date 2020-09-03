f9af29a7b0f6168b34bb9914b94b73560c30e113
Colin González <colin.gonzalez@nomadic-labs.com>
2020-09-03 11:28:53 +0200

    TestCommit: update parsing test

--- gitrawdata/src/test/java/up/visulog/gitrawdata/TestCommit.java
+++ gitrawdata/src/test/java/up/visulog/gitrawdata/TestCommit.java
@@ -1,22 +1,26 @@
 package up.visulog.gitrawdata;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestCommit {
     @Test
-    public void testParse() throws IOException, URISyntaxException {
-        var expected = "Commit{id='6304c1acdc1cbdeb8315528781896abc72a021b8', date='  Tue Sep 1 12:30:53 2020 +0200', author='Aldric Degorre <adegorre@irif.fr>', description='More gradle configuration (with subprojects)'}";
-        var uri = getClass().getClassLoader().getResource("git.log").toURI();
-        try (var reader = Files.newBufferedReader(Paths.get(uri))) {
-            var commit = Commit.parse(reader);
-            assertEquals(expected, commit.toString());
-        }
+    public void testParse() throws IOException {
+	var expected =
+	    "Commit{id='6304c1acdc1cbdeb8315528781896abc72a021b8', date='Tue Sep 1 12:30:53 2020 +0200', author='Aldric Degorre <adegorre@irif.fr>', description='More gradle configuration (with subprojects)\n'}";
+	var builder = new FileRepositoryBuilder();
+	var repo = builder
+	    .readEnvironment() // scan environment GIT_* variables
+	    .findGitDir() // scan up the file system tree
+	    .build();
+	var id = repo.resolve("6304c1acdc");
+        var commit = Commit.parse(repo, id);
+	System.out.println("Hello world");
+	assertEquals(expected, commit.toString());
     }
 }
