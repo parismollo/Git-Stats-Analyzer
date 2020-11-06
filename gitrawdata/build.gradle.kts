0260a85c027deb80f4a88fcf1b28f5de05a39ee3
Colin González <colin.gonzalez@nomadic-labs.com>
2020-11-06 10:49:39 +0100

    Silence SL4J warning

--- gitrawdata/build.gradle.kts
+++ gitrawdata/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 dependencies {
     // https://mvnrepository.com/artifact/org.eclipse.jgit/org.eclipse.jgit
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.8.1.202007141445-r")
+    implementation("org.slf4j:slf4j-nop:1.7.30")
     testImplementation("junit:junit:4.+")
 }
 
