3be196cd9d447df26bc06fcae070e4730a5ca018
jadecrtl <jadecortial@gmail.com>
2021-11-08 16:53:37 +0100

    ajout de la version junit 5.8.1 pour utiliser assertj

--- analyzer/build.gradle.kts
+++ analyzer/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     implementation(project(":config"))
     implementation(project(":gitrawdata"))
     testImplementation("junit:junit:4.+")
+    testImplementation("junit:junit:5.8.1")
 }
 
 
