41c8fc5883e27a7421663557a77fffc3b5804a12
Paris Mollo <parismollochristondis@gmail.com>
2021-11-10 03:12:54 +0100

    update build.gradle file : install javaFX

--- gui/build.gradle.kts
+++ gui/build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
     `java-library`
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.0.9" // Install javaFX
+}
+
+javafx {
+    modules = listOf("javafx.controls", "javafx.fxml", "javafx.web") // get javaFX modules
 }
 
 dependencies {
