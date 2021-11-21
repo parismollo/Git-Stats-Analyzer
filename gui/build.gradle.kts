48f1c10015d1e8d53849bf6843055b9f012eaf8d
Paris Mollo <parismollochristondis@gmail.com>
2021-11-21 19:04:08 +0100

    comment gitrawdata to test code & update kts file

--- gui/build.gradle.kts
+++ gui/build.gradle.kts
@@ -1,12 +1,7 @@
 plugins {
     `java-library`
-    id("application")
-    id("org.openjfx.javafxplugin") version "0.0.9" // Install javaFX
 }
 
-javafx {
-    modules = listOf("javafx.controls", "javafx.fxml", "javafx.web") // get javaFX modules
-}
 
 dependencies {
     testImplementation("junit:junit:4.+")
