0e75dcf77a9d9e0220172031e03d49398a769bb8
Paris Mollo <parismollochristondis@gmail.com>
2021-11-21 21:34:49 +0100

    (gui) add components to TableScreen

--- gui/src/main/java/up/visulog/gui/components/TableComponents.java
+++ gui/src/main/java/up/visulog/gui/components/TableComponents.java
@@ -1,5 +1,53 @@
 package up.visulog.gui.components;
-
+import java.awt.*;
+import java.io.IOException;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 public class TableComponents {
-    
+
+    public static void setFrameAndTable(JFrame frame, String screenTitle, String filePath) throws FontFormatException, IOException {
+        setFrame(frame, filePath, 600, 450);
+        frame.setTitle(screenTitle);  
+        frame.getContentPane().setBackground(new Color(88,205,113));
+        JScrollPane scp = createScroolTable();
+        frame.add(scp);
+    }
+
+    public static JScrollPane createScroolTable() {
+
+        JTable dataTable = new JTable(getTableData(), getTableColNames());
+        dataTable.setBounds(30, 40, 200, 300);
+        JScrollPane sp = new JScrollPane(dataTable);
+
+        return sp;
+    }
+
+    public static void setFrame(JFrame frame, String filePath, int width, int height) {
+        ImageIcon img = new ImageIcon(filePath);
+        frame.setIconImage(img.getImage());
+        frame.setSize(width, height); // 600, 450
+        // setLayout(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+    }
+
+    private static String[][] getTableData() {
+        // TODO: connect to gitrawdata
+        String[][] s ={
+            { "Rick", "4031", "main" },
+            { "Morty", "6014", "test" }
+        };
+        return s;
+    }
+
+    private static String[] getTableColNames() {
+        // TODO: connect to gitrawdata
+        String[] s =
+            { "Member", "Commits", "Branch" };
+        return s;
+    }
 }
---
+++ gui/src/main/java/up/visulog/gui/screens/TableScreen.java
@@ -0,0 +1,17 @@
+package up.visulog.gui.screens;
+
+import java.awt.FontFormatException;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+import up.visulog.gui.components.TableComponents;
+public class TableScreen extends JFrame {
+    public static void main(String[] args) throws FontFormatException, IOException {
+        new TableScreen();
+    }
+
+    TableScreen() throws FontFormatException, IOException {
+        TableComponents.setFrameAndTable(this, "DinoLog - Stats Table", "dinosaur.png");
+    }
+}
