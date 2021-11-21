a1d3f9c5a295abc38150766ad8033eff53a45dfd
Paris Mollo <parismollochristondis@gmail.com>
2021-11-21 19:16:35 +0100

    (gui) add HomeScreen dependecies

---
+++ gui/src/main/java/up/visulog/gui/validators/HomeValidators.java
@@ -0,0 +1,20 @@
+package src.main.java.validators;
+
+import java.io.File;
+
+public class HomeValidators {
+
+	public static boolean isGitFolder(File file_path) {
+		if(file_path == null || !file_path.exists()) {
+			return false;
+		} // Si le dossier n'existe pas, on quitte.
+		File[] files = file_path.listFiles(); // On recupere tous les fichiers et dossiers contenus dans le dossier passé en argurment.
+		for(File file : files) {
+			// On parcourt le tableau.
+			if(file.isDirectory() && file.getName().equals(".git")) {
+				return true;
+			} // Si on trouve un dossier qui a pour nom exacte ".git" alors on renvoie true.
+		}
+		return false; // Si on à pas trouvé de dossier ".git".
+	}
+}
