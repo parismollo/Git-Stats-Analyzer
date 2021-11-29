be78550fe38b8f9bd950327e3390733ae42ed6da
elallam <hafsabondy@gmail.com>
2021-11-29 23:42:39 +0100

    modif de la fonction PluginConfig

--- config/src/main/java/up/visulog/config/PluginConfig.java
+++ config/src/main/java/up/visulog/config/PluginConfig.java
@@ -1,5 +1,44 @@
 package up.visulog.config;
 
+import java.util.HashMap;
+
 // TODO: define what this type should be (probably a Map: settingKey -> settingValue)
-public interface PluginConfig {
+// TODO: voir si c'est mieux de laisser une interface 
+public abstract class PluginConfig {
+    protected HashMap<String, String> pluginConfig;
+
+    public PluginConfig() {} // constructeur vide temporaire
+
+    public PluginConfig(String graph, String date, String startDate, String endDate) {
+        this.pluginConfig = new HashMap<String, String>();
+        pluginConfig.put("graph", graph);
+        pluginConfig.put("date", date);
+        pluginConfig.put("startDate", startDate);
+        pluginConfig.put("endDate", endDate);
+    }
+
+    public HashMap<String, String> getPluginConfig() {
+        return pluginConfig;
+    }
+
+    public boolean isGraph() {
+        return !pluginConfig.get("graph").equals("");
+    }
+
+    public String getGraph() {
+        return pluginConfig.get("graph");
+    }
+
+    public String getDate() {
+        return pluginConfig.get("date"); 
+    }
+
+    public String getStartDate() {
+        return pluginConfig.get("startDate"); 
+    }
+
+    public String getEndDate() {
+        return pluginConfig.get("endDate"); 
+    }
+
 }
