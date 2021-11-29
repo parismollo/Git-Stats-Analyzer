b7b37388c2ac9d032b8c208a7db055a910373935
EL ALLAM hafsa <hafsabondy@gmail.com>
2021-11-29 23:53:36 +0100

    ajout de configCountCommitOnOneDay

---
+++ config/src/main/java/up/visulog/config/configCountCommitOnOneDay.java
@@ -0,0 +1,9 @@
+package up.visulog.config;
+
+public class configCountCommitOnOneDay extends PluginConfig {
+
+    public configCountCommitOnOneDay(String graph, String date) {
+        super(graph, date, "", "");
+    }
+    
+}
