a3148e5f429d97cb12f93b4912525c76b2ed163b
EL ALLAM hafsa <hafsabondy@gmail.com>
2021-11-29 23:54:55 +0100

    ajout de configCountCommit

---
+++ config/src/main/java/up/visulog/config/configCountCommit.java
@@ -0,0 +1,12 @@
+package up.visulog.config;
+
+import java.util.HashMap;
+
+public class configCountCommit extends PluginConfig {
+    public HashMap<String, String> pluginConfig;
+    
+    public configCountCommit(String graph) {
+        super(graph, "", "", "");
+    }
+
+}
