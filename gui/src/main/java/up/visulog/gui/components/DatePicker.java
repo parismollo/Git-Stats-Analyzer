675ca10d5c7c14cf9798dfaa5ad92d8c785dcbc5
Leopold Abignoli <leopold.abignoli@gmail.com>
2021-12-01 22:56:51 +0100

    Ajout d'un DatePicker. Un JPanel avec des selecteurs pour choisir un jour, un mois et une annee.

---
+++ gui/src/main/java/up/visulog/gui/components/DatePicker.java
@@ -0,0 +1,63 @@
+package up.visulog.gui.components;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+public class DatePicker extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
+	public static SimpleDateFormat defaultFormat = new SimpleDateFormat("yyyy-MM-dd");
+	
+	private JComboBox<String> days, months, years;
+	
+	public DatePicker() {
+		days = new JComboBox<String>(getStringList(1, 31));
+		months = new JComboBox<String>(getStringList(1, 12));
+		
+		String[] dateInfos = getDateInfos(new Date());
+		int actualYear = Integer.parseInt(dateInfos[0]);
+		String[] yearsTab = getStringList(1970, actualYear);
+		years = new JComboBox<String>(yearsTab);
+		
+		years.setSelectedIndex(yearsTab.length-1);
+		months.setSelectedIndex(Integer.parseInt(dateInfos[1])-1);
+		days.setSelectedIndex(Integer.parseInt(dateInfos[2])-1);
+		
+		setOpaque(false);
+		add(years);
+		add(months);
+		add(days);
+	}
+		
+	
+	public static String[] getStringList(int start, int end) {
+		if(start > end)
+			return new String[0];
+		String[] tab = new String[end-start+1];
+		for(int i=0;i<end-start+1;i++) {
+			String str = start+i+"";
+			if(str.length() == 1)
+				str = "0"+str;
+			tab[i] = str;
+		}
+		return tab;
+	}
+	
+	public static String[] getDateInfos(Date date) {
+		SimpleDateFormat day = new SimpleDateFormat("dd"),
+						 month = new SimpleDateFormat("MM"),
+						 year = new SimpleDateFormat("yyyy");
+		return new String[] {year.format(date), month.format(date),
+							 day.format(date)};
+	}
+	
+	public String getDate() {
+		return years.getSelectedItem()+"-"+
+			   months.getSelectedItem()+"-"+
+			   days.getSelectedItem();
+	}
+	
+}
