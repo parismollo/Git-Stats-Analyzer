98d64523257af0b755dd9b800217dc4fbb4fdf41
Paris Mollo <parismollochristondis@gmail.com>
2021-12-03 15:40:51 +0100

    (stats) add TestCountMergeCommit - Checks if the function compute the right number of merges

---
+++ analyzer/src/test/java/up/visulog/analyzer/TestCountMergeCommits.java
@@ -0,0 +1,33 @@
+package up.visulog.analyzer;
+
+import org.junit.Test;
+import up.visulog.gitrawdata.Commit;
+import up.visulog.gitrawdata.CommitBuilder;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCountMergeCommits {
+    /* Let's check whetherthe sum of the commits of each weekday is equal to the total number of commits */
+    @Test
+    public void checkBranchSum() {
+        int expectedResult = 3;
+        String[] randomStrings = {"9e74f15 8f836af", "09f9cbd 1329241"};
+        var log = new ArrayList<Commit>();
+        var entries = 20;
+        for (int i = 0; i < entries; i++) {
+            // Create in total 3 merges only
+            if (i == 1 || i == 2) { 
+                log.add(new CommitBuilder("").setMergedFrom(randomStrings[0]).createCommit());
+            }
+            if (i == 3) {
+                log.add(new CommitBuilder("").setMergedFrom(randomStrings[1]).createCommit());
+            }
+            log.add(new CommitBuilder("").createCommit());
+        }
+        var res = CountMergeCommitsPlugin.processLog(log);
+        var sum = res.getTotalMergeCommits();
+        assertEquals(expectedResult, sum);
+    }
+}
