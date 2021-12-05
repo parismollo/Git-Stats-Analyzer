23070028ca2f668a8f492507922037813cb5acab
EL ALLAM hafsa <hafsabondy@gmail.com>
2021-12-05 19:30:45 +0100

    Delete configCountCommitOnOneDay.java

--- config/src/main/java/up/visulog/config/configCountCommitOnOneDay.java
+++
@@ -1,9 +0,0 @@
-package up.visulog.config;
-
-public class configCountCommitOnOneDay extends PluginConfig {
-
-    public configCountCommitOnOneDay(String graph, String date) {
-        super(graph, date, "", "");
-    }
-    
-}
