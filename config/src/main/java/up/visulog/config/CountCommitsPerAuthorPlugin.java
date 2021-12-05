380cd141bbbfc589ba65d78e47d876e7661c5c4c
EL ALLAM hafsa <hafsabondy@gmail.com>
2021-12-05 18:51:49 +0100

    fichier pour le plugin -CommitsPerAuthorPlugin

---
+++ config/src/main/java/up/visulog/config/CountCommitsPerAuthorPlugin.java
@@ -0,0 +1,11 @@
+
+public class CountCommitsPerAuthorPlugin extends PluginConfig {
+	public CountCommitsPerAuthorPlugin() {
+		  super();
+		}
+	public boolean isValid() {
+		  return pluginConfig.get("date").equals("") 
+		    && pluginConfig.get("startDate").equals("")
+		    && pluginConfig.get("endDate").equals("");
+		}
+}
