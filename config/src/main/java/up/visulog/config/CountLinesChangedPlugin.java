fccc3e606b08417a23468cb936980976e8bc6f03
EL ALLAM hafsa <hafsabondy@gmail.com>
2021-12-05 18:52:46 +0100

    fichier pour le plugin -CountLinesChangedPlugin

---
+++ config/src/main/java/up/visulog/config/CountLinesChangedPlugin.java
@@ -0,0 +1,11 @@
+
+public class CountLinesChangedPlugin extends PluginConfig{
+	public CountLinesChangedPlugin() {
+		  super();
+		}
+	public boolean isValid() {
+		  return pluginConfig.get("date").equals("") 
+		    && pluginConfig.get("startDate").equals("")
+		    && pluginConfig.get("endDate").equals("");
+		}
+}
